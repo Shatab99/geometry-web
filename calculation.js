3b2c7c27032974c189aff969614d91f0cb538a6e
shatab <shatabag4749@gmail.com>
2023-08-18 23:57:24 +0600

    updated

--- calculation.js
+++ calculation.js
@@ -11,7 +11,8 @@ function triangle(){
     const inputb = input('tri-b');
     const inputh = input('tri-h');
     const result = .5* inputb * inputh;
-    document.getElementById('area-field').innerText = "The area of Triangle is : "+result + " cm";
+    document.getElementById('area-field').innerText = "Triangle is : "+result ;
+    document.getElementById('tri-res').innerText = result;
 }
 
 
@@ -19,7 +20,8 @@ function rectangle(){
     const w = input('rec-w');
     const l = input('rec-l');
     const result = w*l;
-    document.getElementById('area-field').innerText = "The area of rectangle is : "+result + " cm";
+    document.getElementById('area-field').innerText = "rectangle is : "+result ;
+    document.getElementById('rec-res').innerText = result;
 }
 
 
@@ -27,7 +29,8 @@ function Parallelogram(){
     const b = input('par-b');
     const h = input('par-h');
     const result = b*h;
-    document.getElementById('area-field').innerText = "The area of Parallelogram is : "+result + " cm";
+    document.getElementById('area-field').innerText = "Parallelogram is : "+result ;
+    document.getElementById('pal-res').innerText = result;
 }
 
 
@@ -35,7 +38,8 @@ function Rhombus(){
     const b = input('ram-d1');
     const h = input('ram-d2');
     const result = .5*b*h;
-    document.getElementById('area-field').innerText = "The area of Rhombus is : "+result + " cm";
+    document.getElementById('area-field').innerText = "Rhombus is : "+result ;
+    document.getElementById('ram-res').innerText = result;
 }
 
 
@@ -43,7 +47,8 @@ function Pentagon(){
     const b = input('pen-p');
     const h = input('pen-b');
     const result = .5*b*h;
-    document.getElementById('area-field').innerText = "The area of Pentagon is : "+result + " cm";
+    document.getElementById('area-field').innerText = "Pentagon is : "+result ;
+    document.getElementById('pen-res').innerText = result;
 }
 
 
@@ -51,5 +56,6 @@ function Ellipse(){
     const b = input('ell-a');
     const h = input('ell-b');
     const result = 3.1416*b*h;
-    document.getElementById('area-field').innerText = "The area of Ellipse is : "+result + " cm";
+    document.getElementById('area-field').innerText = "Ellipse is : "+result ;
+    document.getElementById('ell-res').innerText = result;
 }
